334c10805af6098ad4ecd3763735f46989b1ec3f
Juber Patel <juberpatel@gmail.com>
2017-07-20 14:18:37 -0400

    moved countreads part from Marianas to Waltz

---
+++ src/org/mskcc/juber/waltz/Constants.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package org.mskcc.juber.waltz;
+
+/**
+ * @author Juber Patel
+ *
+ */
+public class Constants
+{
+	public static final int readLength = 100;
+	public static final int minMappingQuality = 20;
+
+
+}
